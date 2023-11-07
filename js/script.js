8fe0d0fbd9d9361bfbf6cc221d1fa9588e0f9419
Santiago <povedasantiago250@gmail.com>
2023-11-07 03:27:39 -0500

    Actualización de animación 'Scroll Reveal' para imagenes, barras, y elementos clickables

--- js/script.js
+++ js/script.js
@@ -11,4 +11,66 @@ document.addEventListener("click", function (e) {
         const myModal = new bootstrap.Modal(document.getElementById('gallery-model'));
         myModal.show();
     }
-})
+});
+ScrollReveal().reveal('.barra', {
+    origin: 'left',
+    duration: 1800,
+    distance: '50%',
+    easing: 'ease',
+});
+ScrollReveal().reveal('.imgPersonal', {
+    origin: 'left',
+    duration: 1500,
+    distance: '50%',
+    linear: 'linear',
+});
+ScrollReveal().reveal('.imgExperiencia', {
+    origin: 'right',
+    duration: 1500,
+    distance: '33%',
+    linear: 'linear',
+});
+ScrollReveal().reveal('.imgHabilidad', {
+    origin: 'left',
+    duration: 1500,
+    distance: '33%',
+    linear: 'linear',
+});
+ScrollReveal().reveal('.imgContacto', {
+    origin: 'right',
+    duration: 1500,
+    distance: '33%',
+    linear: 'linear',
+});
+ScrollReveal().reveal('.imgcards', {
+    origin: 'bottom',
+    duration: 700,
+    distance: '33%',
+    linear: 'linear',
+});
+ScrollReveal().reveal('.imgcardsbottom', {
+    origin: 'top',
+    duration: 700,
+    distance: '33%',
+    linear: 'linear',
+});
+ScrollReveal().reveal('.cardPython', {
+    origin: 'left',
+    duration: 700,
+    distance: '33%',
+    linear: 'linear',
+});
+ScrollReveal().reveal('.cardPPT', {
+    origin: 'right',
+    duration: 700,
+    distance: '100%',
+    linear: 'linear',
+});
+ScrollReveal().reveal('.redes', {
+    origin: 'left',
+    duration: 700,
+    distance: '100%',
+    linear: 'linear',
+});
+
+
