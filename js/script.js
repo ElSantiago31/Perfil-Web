b08fb576b76184e833e433863392f103709431de
Santiago <povedasantiago250@gmail.com>
2023-11-06 22:40:25 -0500

    se agrega la sección de certificaciones, (prueba piloto para el responsive design de ese apartado)

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ $(document).ready(function () {
         $(".main").toggleClass("shifted");
     });
 });
-
+document.addEventListener("click", function (e) {
+    if(e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-model'));
+        myModal.show();
+    }
+})
