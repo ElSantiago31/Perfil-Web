06c97d189845fa183571cf96c6ceffb864ba8632
Santiago <povedasantiago250@gmail.com>
2023-11-09 02:42:06 -0500

    Cambios animaciones script.js recorte de codigo

--- js/script.js
+++ js/script.js
@@ -24,53 +24,9 @@ ScrollReveal().reveal('.imgPersonal', {
     distance: '50%',
     linear: 'linear',
 });
-ScrollReveal().reveal('.imgExperiencia', {
-    origin: 'right',
-    duration: 1500,
-    distance: '33%',
-    linear: 'linear',
-});
-ScrollReveal().reveal('.imgHabilidad', {
-    origin: 'left',
-    duration: 1500,
-    distance: '33%',
-    linear: 'linear',
-});
-ScrollReveal().reveal('.imgContacto', {
-    origin: 'right',
-    duration: 1500,
-    distance: '33%',
-    linear: 'linear',
-});
-ScrollReveal().reveal('.imgcards', {
-    origin: 'bottom',
-    duration: 700,
-    distance: '33%',
-    linear: 'linear',
-});
-ScrollReveal().reveal('.imgcardsbottom', {
+ScrollReveal().reveal('.reveal', {
     origin: 'top',
-    duration: 700,
+    duration: 1500,
     distance: '33%',
     linear: 'linear',
 });
-ScrollReveal().reveal('.cardPython', {
-    origin: 'left',
-    duration: 700,
-    distance: '33%',
-    linear: 'linear',
-});
-ScrollReveal().reveal('.cardPPT', {
-    origin: 'right',
-    duration: 700,
-    distance: '100%',
-    linear: 'linear',
-});
-ScrollReveal().reveal('.redes', {
-    origin: 'left',
-    duration: 700,
-    distance: '100%',
-    linear: 'linear',
-});
-
-
